5923c75bff10023398e0f0e8b58999d966293224
John Taggart <taggartj1985@gmail.com>
2020-05-29 15:26:50 +0100

    started fresh so can replan classes

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+    public class PlayerTest {
+        Player player;
+        String name;
+        Card card;
+
+
+        @Before
+        public void before(){
+            name = "John";
+            player = new Player(name);
+            card = new Card(SuitType.HEARTS, RankType.ACE);
+        }
+
+        @Test
+        public void hasName(){
+            assertEquals("John", player.getName());
+        }
+
+        @Test
+        public void hasEmptyHand(){
+            assertEquals(0, player.getHand().size());
+        }
+
+        @Test
+        public void canTakeCard(){
+            player.addCard(card);
+            assertEquals(1, player.getHand().size());
+        }
+
+        @Test
+        public void canClearHand(){
+            player.addCard(card);
+            player.clearHand();
+            assertEquals(0, player.getHand().size());
+        }
+    }
