b6dd58205f8e305bf08eff358112cd8f662284b6
John Taggart <taggartj1985@gmail.com>
2020-05-29 15:28:20 +0100

    player and cards working

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,33 @@
+
+    import org.junit.Before;
+    import org.junit.Test;
+
+    import static org.junit.Assert.assertEquals;
+
+    public class CardTest {
+
+        Card card;
+
+        @Before
+        public void before(){
+            card = new Card(SuitType.HEARTS, RankType.ACE);
+        }
+
+        @Test
+        public void canGetSuit() {
+            assertEquals(SuitType.HEARTS, card.getSuit());
+        }
+
+        @Test
+        public void canGetRank() {
+            assertEquals(RankType.ACE, card.getRank());
+        }
+
+        @Test
+        public void queenHasValue10() {
+            card = new Card(SuitType.HEARTS, RankType.QUEEN);
+            assertEquals(10, card.getValueFromEnum());
+
+        }
+    }
+
